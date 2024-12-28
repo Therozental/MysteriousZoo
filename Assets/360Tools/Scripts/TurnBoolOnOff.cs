b37e1f79fa9851047428407cbd49711ec1e588cb
Therozental <160230574+Therozental@users.noreply.github.com>
2024-12-28 16:53:49 +0200

    rewrite some scripts

--- Assets/360Tools/Scripts/TurnBoolOnOff.cs
+++ Assets/360Tools/Scripts/TurnBoolOnOff.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 // 1) Use the 360Tools library.
 using IST360Tools;
+using UnityEngine.Serialization;
 
 // 2) Our class should inherit from CustomActionScript instead of Monobehavior
 //    Learn more about inheritance here: https://learn.unity.com/tutorial/inheritance
 public class TurnBoolOnOff : CustomActionScript
 {
-    public bool isOn;    // Create a public bool variable to store the state of the object.
+    [FormerlySerializedAs("isOn")] public bool wasInteracted = false;    // Create a public bool variable to store the state of the object.
 
     // Override the DoAction method.
     public override void DoAction()
     {
+        wasInteracted = true;
+        /*
         // Check if the object is on or off and change the state.
-        if (isOn == true)
+        if (wasInteracted == true)
         {
-            isOn = false;
+            wasInteracted = false;
         }
         else
         {
-            isOn = true;
+            wasInteracted = true;
         }
+        */
     }
 }
